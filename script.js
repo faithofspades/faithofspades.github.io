6324538a31cb37d783a6c05309d2c520116a8783
Faith <ardist456@gmail.com>
2025-02-27 23:30:11 -0500

    fixed

--- script.js
+++ script.js
@@ -66,6 +66,10 @@ const lfoGain = myAudioCtx.createGain();
 lfoGain.gain.value = 0.002;
 lfo.connect(lfoGain);
 lfoGain.connect(delay.delayTime);
+startAudio = function() {
+  audioElement.play();
+  myAudioCtx.resume();
+}
 
 //Add event listener to button
 let myButton = document.getElementById('startAudio');
